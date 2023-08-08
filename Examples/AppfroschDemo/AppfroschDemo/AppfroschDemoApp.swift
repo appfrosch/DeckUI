a76f1ac0006af40d83ab4542d4d3dc1982d16f54
Andreas Seeger <splash@appfros.ch>
2023-08-08 07:46:06 +0200

    ADD Color extension: Codable

---
+++ Examples/AppfroschDemo/AppfroschDemo/AppfroschDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppfroschDemoApp.swift
+//  AppfroschDemo
+//
+//  Created by Andreas Seeger on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppfroschDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
